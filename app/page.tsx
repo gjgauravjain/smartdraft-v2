5f83deb4ebae9e31afe43bb75f103b2ba100cda8
rohit2698 <rohit.thakur2698@gmail.com>
2026-06-01 10:32:57 +0530

    Fixed build error

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { Header } from "@/components/layout/Header";
+import { Header } from "@/components/layout/header";
 import { HomeContent } from "../components/home/home-content";
 
 export default function Home() {
